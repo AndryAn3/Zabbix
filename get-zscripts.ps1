4664fb8e691233a9796bc4a1d3091a4ef22d3635
AndryAn3 <andryan3@gmail.com>
2021-05-21 15:20:21 +0200

    kj

--- get-zscripts.ps1
+++ get-zscripts.ps1
@@ -5,14 +5,14 @@
 #$cert=Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
 #$cert = Get-PfxCertificate -FilePath C:\Test\Mysign.pfx
 #Set-AuthenticodeSignature -FilePath ServerProps.ps1 -Certificate $cert
-
+try {
 $path = "C:\Program Files\Zabbix Agent 2\Scripts\"
 $repository =  "https://raw.githubusercontent.com/AndryAn3/Zabbix/main/"
 If(!(test-path $path)){ New-Item -ItemType Directory -Force -Path $path }
 remove-item -Path "$path\ps.xlm" -Force -Confirm:$false  
 gci $path | % { $_.name } >> "$path\ps.xlm"
 #Get-AuthenticodeSignature -FilePath $Env:TEMP\list.xxl
-$apple = Invoke-WebRequest "https://raw.githubusercontent.com/AndryAn3/Zabbix/main/list.xxl"  #webaddress later
+$apple = Invoke-WebRequest "https://raw.githubusercontent.com/AndryAn3/Zabbix/main/list.xxl" 
 $orage = get-content "C:\Program Files\Zabbix Agent 2\scripts\ps.xlm"
 remove-item -path "$env:TEMP\lkjhhfgd.txt" -Force -Confirm:$false  
 $apple.content | Set-Content "$env:TEMP\lkjhhfgd.txt"
@@ -20,10 +20,18 @@ $apple = get-Content "$env:TEMP\lkjhhfgd.txt"
 $DLlist = Compare-Object $apple $orage -PassThru | Where-Object {$_.SideIndicator -ne "=>"}
     foreach ($item in $DLlist) {
         $test =  "$repository" + "$item"
+        $xpath =  "$path" + "$item"
         $WebClient = New-Object System.Net.WebClient
-	$WebClient.DownloadFile("$test","$path")  #"$path + $item"
-    write-host $test
-    }
-   # catch [System.Net.WebException],[System.IO.IOException] {        "An error occurred. Files were not downloaded."   }
+    	$WebClient.DownloadFile("$test","$xpath")
+        $downloaded += "$item"
+     }
+}
+catch {
+    [System.Net.WebException],[System.IO.IOException] {        "An error occurred. Files $_ were not downloaded."   }
+}
+Finally {
+    remove-item -path "$env:TEMP\lkjhhfgd.txt" -Force -Confirm:$false  
+    remove-item -Path "$path\ps.xlm" -Force -Confirm:$false  
+}
 
    
