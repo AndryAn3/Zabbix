c766e0e7f40c7c639b378cdb15e128f527248929
AndryAn3 <andryan3@gmail.com>
2021-07-16 16:20:37 +0200

    final tls?

--- tls.ps1
+++ tls.ps1
@@ -1,6 +1,15 @@
 ### Created by Andry: ‎vrijdag ‎16 ‎juli ‎2021, ‏‎13:37:32
 ### last change ‎vrijdag ‎16 ‎juli ‎2021, ‏‎14:54:35 
-$key = -join (((48..57)+(65..90)+(97..122)) * 80 |Get-Random -Count 32 |%{[char]$_})
+#$key = -join (((48..57)+(65..90)+(97..122)) * 80 |Get-Random -Count 32 |%{[char]$_})
+function Get-RandomHex {
+    param(
+        [int] $Bits = 256
+    )
+    $bytes = new-object 'System.Byte[]' ($Bits/8)
+    (new-object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($bytes)
+    (new-object System.Runtime.Remoting.Metadata.W3cXsd2001.SoapHexBinary @(,$bytes)).ToString()
+}
+$key= Get-RandomHex -Bits 256
 $hostFQDN = ([System.Net.Dns]::GetHostByName(($env:computerName))).HostName
 $ZabbixConfig = "zabbix_agent2.conf"
 $GetFile = Get-Content -Path "c:\program files\Zabbix Agent 2\$ZabbixConfig"
@@ -11,11 +20,8 @@ if ($id.trim().StartsWith("#")) {
     elseif ([string]::IsNullOrEmpty($id)) {        add-content -Path "c:\program files\Zabbix Agent 2\$ZabbixConfig" -value "`n$iid"
     }
     #else {     write-host "ID is something else, or already done ofc $id"     }   safety checked
-[string]$pskey = $getfile | Select-String -pattern "TLSPSKVALUE="
-$psid = "TLSPSKVALUE=$key"
-if ($pskey.trim().StartsWith("#")) { 
-    $GetFile -replace $pskey, $psid  |  Set-Content -Path "c:\program files\Zabbix Agent 2\$ZabbixConfig"  }
-    elseif ([string]::IsNullOrEmpty($pskey)) {     add-content -Path "c:\program files\Zabbix Agent 2\$ZabbixConfig" -value "`n$psid" } 
+    New-Item -Path "c:\program files\Zabbix Agent 2\psk.psk" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    Set-Content -Path "c:\program files\Zabbix Agent 2\psk.psk" -Value "$key"
     #else {     write-host "Noooooooooo  keyvalue $pskey"    }  safety checked
 [string]$tlsa = $getfile | Select-String -pattern "TLSAccept="
 $tlsab = "TLSAccept=psk"
@@ -35,5 +41,3 @@ if ($tlsc.trim().StartsWith("#")) {
     add-content -Path "c:\program files\Zabbix Agent 2\$ZabbixConfig" -value "`n$tlsca"
 } 
 write-host "Use the following PSK Key for the given host: $key"
-sleep -Seconds 10
-Restart-Service -Name 'Zabbix Agent 2'  -Force
